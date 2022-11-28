13ad82d7abf3692f4a2964e76e9b0bef4977e11b
Ngọc Thắng <ngocthang.devweb@gmail.com>
2022-11-28 15:40:18 +0700

    update

--- js/cart.js
+++ js/cart.js
@@ -1,38 +1,31 @@
+const products = JSON.parse(localStorage.getItem('products')) || []
+const productJS = document.querySelector('.product-js')
 const totalPrice = document.querySelector('.total-price')
-const productJs = document.querySelector('.product-js')
-const account = JSON.parse(localStorage.getItem('account'))
-const arr = JSON.parse(localStorage.getItem('product'))
 let deleteBtn
 
 function render() {
-    if (!isLogin) {
-        alert('Hãy đăng nhập')
-        window.location = ('./login.html')
-    }
-    let htmls = arr?.reduce((preVal, curVal) => {
-        return preVal + `
-                <div class="cart-product">
-                    <img src=${curVal.img} alt="" class="cart-product-img">
-                    <div class="product-info">
-                        <h3 class="product-name">${curVal.name}</h3>
-                        <p value=${curVal.price} class="product-price">${parseInt(curVal.price).toLocaleString('it-IT', {style : 'currency', currency : 'VND'})}</p>
-                        <div class="cart-product-btn">
-                            <span>-</span>
-                            <input disabled type="text" min="1" value="1" class="quantity">
-                            <span>+</span>
-                        </div>
-                    </div>
-                    <button class="delete-product">XÓA SẢN PHẨM</button>
+    let htmls = products.reduce((pre, cur) => {
+        return pre + `
+        <div class="cart-product">
+            <img src=${cur.path} alt="" class="cart-product-img">
+            <div class="product-info">
+                <h3 class="product-name">${cur.name}</h3>
+                <p value="${cur.price}" class="product-price">${parseInt(cur.price).toLocaleString('it-IT', {style : 'currency', currency : 'VND'})}</p>
+                <div class="cart-product-btn">
+                    <span>-</span>
+                    <input type="text" min="1" value="1" class="quantity">
+                    <span>+</span>
                 </div>
-            `
+            </div>
+            <button class="delete-product">XÓA SẢN PHẨM</button>
+        </div>
+        `
     }, '<h3 class="cart-title">THÔNG TIN SẢN PHẨM</h3>')
-    if (!htmls) htmls = 'Bạn chưa chọn sản phẩm nào'
-    productJs.innerHTML = htmls
 
+    productJS.innerHTML = htmls
     deleteBtn = document.querySelectorAll('.delete-product')
     handleClick()
 }
-
 function calTotalPrice() {
     let price = 0;
     const listPrice = document.querySelectorAll('.product-price')
@@ -62,28 +55,23 @@ function handleClick() {
         }
     })
 }
-render()
 
+render() 
 handleClick()
-
 calTotalPrice()
-
-// deleteBtn.onclick = () => {
-    // console.log(deleteBtn[0].previousElementSibling.childNodes[1].innerText);
-// }
 
 deleteBtn.forEach(btn => {
     btn.onclick = () => {
         const strName = btn.previousElementSibling.childNodes[1].innerText
         let index
-        for(let i = 0; i < arr.length; i++) {
-            if (arr[i].name == strName) {
+        for(let i = 0; i < products.length; i++) {
+            if (products[i].name == strName) {
                 index = i
                 break
             }
         }
-        arr.splice(index, 1)
-        localStorage.setItem('product', JSON.stringify(arr))
+        products.splice(index, 1)
+        localStorage.setItem('products', JSON.stringify(products))
         location.reload()
     }
 })
--- js/product.js
+++ js/product.js
@@ -1,129 +1,16 @@
-const productClassic = [
-    {
-        name: 'Amazing Bar Chocolate Almond',
-        price: '89.300đ',
-        path: '../img/Product/TruyenThong/Amazing-Bar-Chocolate-Almond.png',
-        detailPath: '../html/products/amazing-almond.html'
-    },
-    {
-        name: 'Amazing Bar Chocolate 85gr',
-        price: '89.300đ',
-        path: '../img/Product/TruyenThong/Amazing-Bar-Chocolate-85.png',
-        detailPath: '../html/products/amazing-bar.html'
-    },
-    {
-        name: 'Amazing Bar Chocolate Milk',
-        price: '89.300đ',
-        path: '../img/Product/TruyenThong/Amazing-Bar-Chocolate-Milk.png',
-        detailPath: '../html/products/amazing-milk.html'
-    },
-    {
-        name: 'Bitter Chocolate',
-        price: '89.300đ',
-        path: '../img/Product/TruyenThong/Bar100-Chocolate-78-500x500.jpg',
-        detailPath: '../html/products/bitter-chocolate.html'
-    },
-    {
-        name: 'Forest Chocolate',
-        price: '89.300đ',
-        path: '../img/Product/TruyenThong/Forest-Chocolate-001-500x500.jpg',
-        detailPath: '../html/products/forest-chocolate.html'
-    },
-    {
-        name: 'Matcha Chocolate',
-        price: '89.300đ',
-        path: '../img/Product/TruyenThong/Bar100-Chocolate-Matcha-500x500.jpg',
-        detailPath: '../html/products/matcha-chocolate.html'
-    },
-    {
-        name: 'White Chocolate',
-        price: '89.300đ',
-        path: '../img/Product/TruyenThong/Bar100-Chocolate-White-500x500.jpg',
-        detailPath: '../html/products/white-chocolate.html'
-    },
-]
+const orderBtn = document.querySelector('.product-btn')
+const products = JSON.parse(localStorage.getItem('products')) || []
 
-const productLove = [
-    {
-        name: 'Garden Chocolate Love',
-        price: '89.300đ',
-        path: '../img/Product/Love/Garden-Chocolate-001-500x500.jpg',
-        detailPath: '../html/productLove/garden-love.html'
-    },
-    {
-        name: 'Heart Box Love',
-        price: '89.300đ',
-        path: '../img/Product/Love/heart-box-chocolate.png',
-        detailPath: '../html/productLove/heart-box.html'
-    },
-    {
-        name: 'Love Chocolate Blue',
-        price: '89.300đ',
-        path: '../img/Product/Love/love-chocolate-blue.jpg',
-        detailPath: '../html/productLove/love-blue.html'
-    },
-    {
-        name: 'Love Chocolate (V2)',
-        price: '89.300đ',
-        path: '../img/Product/Love/love-chocolate.jfif',
-        detailPath: '../html/productLove/love-chocolate-2.html'
-    },
-    {
-        name: 'Love Chocolate',
-        price: '89.300đ',
-        path: '../img/Product/Love/love-chocolate-1.jpg',
-        detailPath: '../html/productLove/love-chocolate.html'
-    },
-    {
-        name: 'Love Chocolate (V3)',
-        price: '89.300đ',
-        path: '../img/Product/Love/valentine-chocolate-1.jfif',
-        detailPath: '../html/productLove/valentine-v3.html'
-    },
-    {
-        name: 'Valentine Chocolate',
-        price: '89.300đ',
-        path: '../img/Product/Love/valentine-chocolate.jpg',
-        detailPath: '../html/productLove/valentine.html'
-    },
-]
-
-const product = productClassic.concat(productLove)
-const productJs = document.querySelector('.product-js')
-const productTypes = document.querySelectorAll('.product-type')
-
-function render(product) {
-    const htmls = product.reduce((preVal, curVal) => {
-        return preVal + `
-                <div class="col l-3 m-4 c-6" style="animation: sideOn 0.4s linear forwards;">
-                    <a href=${curVal.detailPath} class="product-item">
-                        <div class="product-img-wrap">
-                            <img src=${curVal.path} alt="" class="product-img">
-                        </div>
-                        <p class="product-name">${curVal.name}</p>
-                        <p class="product-price">${curVal.price}</p>
-                    </a>
-                </div>
-            `
-    }, '')
-    productJs.innerHTML = htmls
-}
-
-
-productTypes.forEach((productType, index) => {
-    productType.onclick = () => {
-        // Remove active class
-        productTypes.forEach(productType => {
-            productType.classList.remove('active')
+orderBtn.addEventListener('click', function() {
+    const name = orderBtn.previousElementSibling.parentElement.childNodes[1].innerText
+    const isExist = products.find(product => product.name == name)
+    if (!isExist) {
+        products.push({
+        path: orderBtn.previousElementSibling.parentElement.previousElementSibling.childNodes[1].src,
+        name,
+        price: orderBtn.previousElementSibling.getAttribute('value')
         })
-        productType.classList.add('active')
-        
-        if (index == 1) {
-            render(productClassic)
-        }
-        else if (index == 2) render(productLove)
-        else render(product)
     }
-})
 
-render(product)
+    localStorage.setItem('products', JSON.stringify(products))
+})
